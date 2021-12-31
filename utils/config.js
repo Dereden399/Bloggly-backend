98eb713d832201442d900cad258d4b7b7827cf3f
Dereden399 <denisk399@gmail.com>
2021-12-31 12:26:04 +0300

    port added

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 
-let PORT = process.env.PORT
+let PORT = process.env.PORT || 3001
 let MONGODB_URI =
   process.env.NODE_ENV == "test"
     ? process.env.MONGODB_URI_TEST
